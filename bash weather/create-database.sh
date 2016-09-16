9663e1b6282e3574d04f403c3810a4c2e24fea5c
TheJixel <draguls@gmail.com>
2016-09-16 10:22:46 +0200

    Update create-database.sh

--- bash weather/create-database.sh
+++ bash weather/create-database.sh
@@ -3,5 +3,5 @@ mysql --local-infile --user=root --password=rootpass  <<STOP
 drop database if exists weatherdb;
 create database if not exists weatherdb;
 use weatherdb;
-create table items (ICAO Varchar(4), time Varchar(16), report Varchar(100));
+create table metar (ICAO Varchar(4), time Varchar(16), report Varchar(100));
 STOP
