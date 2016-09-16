f29a073676404b70131db2ac74fd68cacb571eb3
Joakim Koed <joakimkoed@gmail.com>
2016-09-16 09:52:55 +0200

    Update weather.sh

--- bash weather/weather.sh
+++ bash weather/weather.sh
@@ -3,11 +3,11 @@ CT=$(date -u +%H)
 URL="http://tgftp.nws.noaa.gov/data/observations/metar/cycles/"$CT"Z.TXT"
 # They will get overwritten every 24 hours
 
-if  [ -d "home/vooze/lol/what" ]; then
-wget -O "/home/vooze/lol/what/"$CT"Z.TXT" $URL
-echo "Mappen 'what' findes allerede"
+if  [ -d "~/weather" ]; then
+wget -O "~/weather/"$CT"Z.TXT" $URL
+echo "The folder 'weather' already exists"
 else
-mkdir "/home/vooze/lol/what"
-wget -O "/home/vooze/lol/what/"$CT"Z.TXT" $URL
-echo "folder 'what' did not exist, so it was created."
+mkdir "~/weather"
+wget -O "~/weather/"$CT"Z.TXT" $URL
+echo "The folder 'weather' did not exist, so it was created."
 fi
