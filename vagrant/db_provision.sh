277b183cc0b90a92eda1d681e74387c8ad887202
Joakim Koed <joakimkoed@gmail.com>
2016-09-19 14:42:21 +0200

    Update db_provision.sh

--- vagrant/db_provision.sh
+++ vagrant/db_provision.sh
@@ -6,4 +6,7 @@ restart mysql
 mysqladmin -u root password rootpass
 mysql -uroot mysql --password=rootpass <<< "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rootpass' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 mysql -uroot mysql --password=rootpass <<< "GRANT SELECT ON *.* TO 'read-user'@'%' IDENTIFIED BY 'Passw0rd';"
-bash /vagrant/db_bash.sh
+git clone https://github.com/vooze/automation-project
+bash /home/vagrant/automation-project/bash-weather/create-database.sh
+bash /home/vagrant/automation-project/bash-weather/weather.sh
+(crontab -l 2>/dev/null; echo "10 * * * * bash /home/vagrant/automation-project/bash-weather/weather.sh") | crontab -
