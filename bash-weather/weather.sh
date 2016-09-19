b53dc8f5fe15c8ef8f20bb66c8d58bbd7cbf09fd
Joakim Koed <joakimkoed@gmail.com>
2016-09-19 10:29:36 +0200

    Update weather.sh

--- bash-weather/weather.sh
+++ bash-weather/weather.sh
@@ -4,10 +4,10 @@ URL="http://tgftp.nws.noaa.gov/data/observations/metar/cycles/"$CT"Z.TXT"
 # They will get overwritten every 24 hours
 
 if  [ -d "/home/vagrant/weather" ]; then
-wget -O "/home/vagrant/"$CT"Z.TXT" $URL
+wget -O "/home/vagrant/weather/"$CT"Z.TXT" $URL
 echo "The folder 'weather' already exists"
 else
-mkdir "/home/vagrant/"
-wget -O "/home/vagrant/"$CT"Z.TXT" $URL
+mkdir "/home/vagrant/weather"
+wget -O "/home/vagrant/weather/"$CT"Z.TXT" $URL
 echo "The folder 'weather' did not exist, so it was created."
 fi
