0ea76f986935a28bde256719125a77c3fb62059e
Joakim Koed <joakimkoed@gmail.com>
2016-09-19 08:45:14 +0200

    Update create-database.sh

--- bash weather/create-database.sh
+++ bash weather/create-database.sh
@@ -4,4 +4,5 @@ drop database if exists weatherdb;
 create database if not exists weatherdb;
 use weatherdb;
 create table metar (icao Varchar(4), time Varchar(16), report Varchar(250));
+GRANT SELECT ON *.* TO 'read-user'@'%' IDENTIFIED BY 'Passw0rd';
 STOP
