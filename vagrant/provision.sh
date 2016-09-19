f5bc1a68c7704a6235237ba731c43c3829e43e0a
Joakim Koed <joakimkoed@gmail.com>
2016-09-19 14:43:06 +0200

    Update provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,4 +4,5 @@ apt-get update >/dev/null 2>&1
 apt-get install -y apache2 php5 php5-mysql git >/dev/null 2>&1
 rm -rf /var/www/html
 ln -fs /vagrant/vagrant-html /var/www/html
-bash /vagrant/web_bash.sh
+git clone https://github.com/vooze/automation-project
+cp /home/vagrant/automation-project/vagrant/report_json.php /vagrant/vagrant-html
