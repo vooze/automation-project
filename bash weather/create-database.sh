6c911cd8645551f100a4820fa6cf3c0933547598
Joakim Koed <joakimkoed@gmail.com>
2016-09-19 08:53:58 +0200

    Removed old folder

--- bash weather/create-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mysql --local-infile --user=root --password=rootpass  <<STOP
-drop database if exists weatherdb;
-create database if not exists weatherdb;
-use weatherdb;
-create table metar (icao Varchar(4), time Varchar(16), report Varchar(250));
-GRANT SELECT ON *.* TO 'read-user'@'%' IDENTIFIED BY 'Passw0rd';
-STOP
